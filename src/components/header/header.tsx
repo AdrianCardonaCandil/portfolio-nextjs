97889bd5b8f61e44b25dfce727a181b820318135
Adrian Cardona Candil <acardona@sic4change.org>
2025-11-04 23:32:13 +0000

    En la versión de mobile, me gustaría señalar un cambio adicional a la se

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import { HeaderProfileImage } from "./header-profile-image";
 export function Header() {
   return (
     <section className="w-full bg-card/50 py-24 md:py-32">
-      <div className="container mx-auto grid grid-cols-1 items-center gap-12 px-4 md:grid-cols-2 md:px-8">
+      <div className="container mx-auto grid grid-cols-1 items-center gap-16 px-4 md:grid-cols-2 md:gap-12 md:px-8">
         <HeaderInfo />
         <HeaderProfileImage />
       </div>
