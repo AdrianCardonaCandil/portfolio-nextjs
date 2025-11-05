d33059ff190a96b8fb61d9454eee5b04cc0ef9f6
Adrian Cardona Candil <acardona@sic4change.org>
2025-11-05 19:12:28 +0000

    added definitions for data models

---
+++ src/lib/definitions.ts
@@ -0,0 +1,23 @@
+// This file contains type definitions for your data.
+import type { IconType } from "react-icons";
+
+export type Job = {
+    title: string;
+    company: string;
+    period: string;
+    description: string;
+};
+
+export type Project = {
+    title: string;
+    description: string;
+    technologies: IconType[];
+    image: {
+        src: string;
+        width: number;
+        height: number;
+        hint: string;
+    };
+    githubUrl: string;
+    colSpan?: number;
+};
